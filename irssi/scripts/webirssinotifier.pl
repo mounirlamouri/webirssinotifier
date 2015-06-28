52b3fcd9302db6641a0c01d4e9b94695ae72bf08
Mounir Lamouri <mounir@lamouri.fr>
2015-06-28 12:01:40 +0100

    IRSSI plugin ready with away/idle options.

--- irssi/scripts/webirssinotifier.pl
+++ irssi/scripts/webirssinotifier.pl
@@ -1,45 +1,88 @@
-# Notify of public and private pings via push messages.
+use strict;
+use warnings;
 
 use Irssi;
 use vars qw($VERSION %IRSSI);
 
-$VERSION = "0.0";
-%IRSSI = (
-  authors     => 'Mounir Lamouri <mounir@lamouri.fr>',
+our $VERSION = "0.8";
+our %IRSSI = (
+  authors     => 'Mounir Lamouri',
+  contact     => 'mounir@lamouri.fr',
   name        => "webirssinotifier",
-  description => "Notify of public and private pings via push messages.",
-  license     => "Apache v2.0",
+  description => "Notify of IRC pings via push messages in your browser.",
+  license     => "Apache-2.0",
   url         => "https://github.com/mounirlamouri/webirssinotifier/",
 );
 
-$USER = "email\@address";
+# Users must set webirssinotifier_user to the user that should receive the push
+# notifications.
+# /set webirssinotifier_user <USER>
 
-# TODO:
-# * Have an option to only send push if the user is away on the server.
-# * Show nicks, probably by tracking 'private' signal.
+# Users can use webirssinotifier_away_only to only get push notifications when
+# they are away:
+# /set webirssinotifier_away_only on
+
+# Users can use webirssinotifier_idle_time to only get push notifications when
+# they have been inactive for longer than X seconds.
+# /set webirssinotifier_idle_time X
+
+our $keyPressedTimestamp = time;
+
+sub should_send_message {
+  my ($server) = @_;
+
+  if (Irssi::settings_get_bool('webirssinotifier_away_only') &&
+      !$server->{usermode_away}) {
+    return 0;
+  }
+
+  return Irssi::settings_get_int('webirssinotifier_idle_time') < (time - $keyPressedTimestamp);
+}
+
+sub send_message {
+  my ($type, $name, $body) = @_;
+
+  my $user = Irssi::settings_get_str('webirssinotifier_user');
+  `curl --silent --header "Content-Type: application/json" https://webirssinotifier.appspot.com/send -d "{\\"user\\": \\"${user}\\", \\"type\\": \\"$type\\", \\"name\\": \\"${name}\\", \\"body\\": \\"${body}\\"}" > /dev/null 2>&1 `;
+}
 
 sub signal_print_text {
   my ($dest, $text, $stripped) = @_;
-  my $opt = MSGLEVEL_HILIGHT|MSGLEVEL_MSGS;
-  my $channel = '';
 
-  if (($dest->{level} & ($opt)) &&
-      ($dest->{level} & MSGLEVEL_NOHILIGHT) == 0) {
-    if ($dest->{level} & MSGLEVEL_PUBLIC) {
-      $channel = $dest->{target};
-    }
+  if ((($dest->{level} & MSGLEVEL_PUBLIC) == 0) ||
+      (($dest->{level} & MSGLEVEL_NOHILIGHT) != 0) ||
+      (($dest->{level} & MSGLEVEL_HILIGHT) == 0)) {
+    return;
+  }
 
-    $text = Irssi::strip_codes($text);
-    $result = `curl --silent --header "Content-Type: application/json" https://webirssinotifier.appspot.com/send -d "{\\"user\\": \\"${USER}\\", \\"channel\\": \\"${channel}\\", \\"text\\": \\"${text}\\"}" > /dev/null 2>&1 `;
+  if (should_send_message($dest->{server})) {
+    send_message("channel", $dest->{target}, Irssi::strip_codes($text));
   }
 }
 
-#sub signal_message_private {
-#  my ($msg, $nick, $address) = @_;
-#
-#  $msg = Irssi::strip_codes($msg);
-#  $result = `curl --silent --header "Content-Type: application/json" https://webirssinotifier.appspot.com/send -d "{\\"user\\": \\"${USER}\\", \\"channel\\": \\"\\", \\"nick\\": \\"${nick}\\", \\"text\\": \\"${msg}\\"}" > /dev/null 2>&1 `;
-#}
+sub signal_message_private {
+  my ($server, $msg, $nick, $address) = @_;
+
+  if (should_send_message($server)) {
+    send_message("query", $nick, Irssi::strip_codes($msg));
+  }
+}
+
+sub signal_key_pressed {
+  $keyPressedTimestamp = time;
+}
+
+sub signal_setup_changed {
+  Irssi::signal_remove('gui key pressed', 'signal_key_pressed');
+  if (Irssi::settings_get_int('webirssinotifier_idle_time') > 0) {
+    Irssi::signal_add('gui key pressed', 'signal_key_pressed');
+  }
+}
+
+Irssi::settings_add_str('webirssinotifier', 'webirssinotifier_user', '');
+Irssi::settings_add_int('irssinotifier', 'webirssinotifier_idle_time', 0);
+Irssi::settings_add_bool('webirssinotifier', 'webirssinotifier_away_only', 0);
 
 Irssi::signal_add('print text', 'signal_print_text');
-#Irssi::signal_add('message private', 'signal_message_private');
+Irssi::signal_add('message private', 'signal_message_private');
+Irssi::signal_add('setup changed', 'signal_setup_changed');
