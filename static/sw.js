7768c1fa355f904ef686891d6e7bc9b1e35acaa3
Mounir Lamouri <mounir@lamouri.fr>
2016-04-06 15:17:45 +0100

    Avoid spamming when there are a lot of unread messages.

--- static/sw.js
+++ static/sw.js
@@ -3,24 +3,32 @@ importScripts('helpers.js');
 self.addEventListener('push', function(event) {
   event.waitUntil(getMessages().then(function(messages) {
     var promises = [];
-    messages.forEach(function(message) {
-      if (!message.type)
-        return;
-      var title = '';
-      switch (message.type) {
-        case 'channel':
-          title = 'New ping in ' + message.name;
-          break;
-        case 'query':
-          title = 'New message from ' + message.name;
-          break;
-      }
-
+    if (messages.length > 5) {
+      var title = messages.length + ' new messages';
       promises.push(self.registration.showNotification(title, {
-        body: decodeURI(message.body),
+        body: title,
         icon: '/images/200px-Irssi_logo.png',
       }));
-    });
+    } else {
+      messages.forEach(function(message) {
+        if (!message.type)
+          return;
+        var title = '';
+        switch (message.type) {
+          case 'channel':
+            title = 'New ping in ' + message.name;
+            break;
+          case 'query':
+            title = 'New message from ' + message.name;
+            break;
+        }
+
+        promises.push(self.registration.showNotification(title, {
+          body: decodeURI(message.body),
+          icon: '/images/200px-Irssi_logo.png',
+        }));
+      });
+    }
     return Promise.all(promises);
   }));
 });
